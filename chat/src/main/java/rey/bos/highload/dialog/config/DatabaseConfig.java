96bdf3eb7c5bef0660e5e58bce6d76a6f594c4db
reybos <andreybosiy@yandex.ru>
2024-08-25 17:04:12 +0200

    refactor names and endpoint contract

--- chat/src/main/java/rey/bos/highload/dialog/config/DatabaseConfig.java
+++ chat/src/main/java/rey/bos/highload/dialog/config/DatabaseConfig.java
@@ -1,4 +1,4 @@
-package rey.bos.highload.chat.config;
+package rey.bos.highload.dialog.config;
 
 import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.context.annotation.Bean;
@@ -8,7 +8,7 @@ import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 import javax.sql.DataSource;
 
 @Configuration
-@EnableJdbcRepositories(basePackages = "rey.bos.highload.chat.io.repository")
+@EnableJdbcRepositories(basePackages = "rey.bos.highload.dialog.io.repository")
 public class DatabaseConfig {
 
     @Bean
---
+++ chat/src/main/java/rey/bos/highload/dialog/controller/DialogController.java
@@ -0,0 +1,10 @@
+package rey.bos.highload.dialog.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@RequiredArgsConstructor
+@Controller
+public class DialogController {
+
+}
